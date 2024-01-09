f120596d37f1602cb1c74735d52fbd599b99e4a6
afonsotorrao <115576740+afonsotorrao@users.noreply.github.com>
2024-01-09 17:39:27 +0100

    Update db on seed file and reseed

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,13 @@
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config()
+}
+
 const mongoose = require('mongoose')
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const {descriptors, places} = require('./seedHelpers')
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const dbUrl = process.env.DB_URL
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection;
     db.on('error', console.error.bind(console, "connection error:"))
